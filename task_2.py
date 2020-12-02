69070c6c2409cb189d742822a80f296bb5c53c61
VladKharkevich <kharkevichvlad@gmail.com>
2020-12-02 17:20:00 +0300

    Added functools.total_ordering, added dict 'word_to_number_version' with meaning of word version, in method _validate_patch_version magic indexes was removed

--- task_2.py
+++ task_2.py
@@ -1,7 +1,18 @@
+import functools
 import re
 from typing import List
 
+word_to_number_version = {
+    'a': '-3',
+    'alpha': '-3',
+    'b': '-2',
+    'beta': '-2',
+    'c': '-1',
+    'rc': '-1',
+}
 
+
+@functools.total_ordering
 class Version:
     def __init__(self, version):
         self.version = version
@@ -24,46 +35,39 @@ class Version:
         list_version = self._validate_patch_version(list_version)
         for i in range(5 - len(list_version)):
             list_version.append('0')
-        for i in range(3, 5):
-            if list_version[i] == 'alpha' or list_version[i] == 'a':
-                list_version[i] = '-3'
-            if list_version[i] == 'beta' or list_version[i] == 'b':
-                list_version[i] = '-2'
-            if list_version[i] == 'rc' or list_version[i] == 'c':
-                list_version[i] = '-1'
+        list_version = list(
+            map(self._transform_word_version_to_number, list_version))
         return [int(elem) for elem in list_version]
+
+    def _transform_word_version_to_number(self, version: str) -> str:
+        return word_to_number_version.get(version, version)
 
     def _validate_patch_version(self, list_version: List[str]) -> List[str]:
         """
             Validate situations like 1.8.1b5 to 1.8.1.b.5
         """
-        if not list_version[2].isdigit():
+        index_of_patch_version = 2
+        index_of_pre_release_version = 3
+        index_of_qualifier_version = 4
+
+        if not list_version[index_of_patch_version].isdigit():
             expression = re.compile("([0-9]+)([a-z]+)([0-9]*)")
-            match_groups = expression.match(list_version[2])
+            match_groups = expression.match(
+                list_version[index_of_patch_version])
             if match_groups:
-                list_version[2] = match_groups.group(1)
-                list_version.insert(3, match_groups.group(2))
+                list_version[index_of_patch_version] = match_groups.group(1)
+                list_version.insert(
+                    index_of_pre_release_version, match_groups.group(2))
                 if (match_groups.group(3) != ''):
-                    list_version.insert(4, match_groups.group(3))
+                    list_version.insert(
+                        index_of_qualifier_version, match_groups.group(3))
         return list_version
 
     def __eq__(self, other):
         return self._is_equal(self.version, other.version)
 
-    def __ne__(self, other):
-        return not self._is_equal(self.version, other.version)
-
     def __lt__(self, other):
         return self._is_lower(self.version, other.version)
-
-    def __le__(self, other):
-        return self._is_lower(self.version, other.version) or self._is_equal(self.version, other.version)
-
-    def __gt__(self, other):
-        return not (self._is_lower(self.version, other.version) or self._is_equal(self.version, other.version))
-
-    def __ge__(self, other):
-        return not self._is_lower(self.version, other.version)
 
 
 def main():
@@ -75,9 +79,8 @@ def main():
         ("1.0.1b", "1.0.10-alpha.beta"),
         ("1.0.0-rc.1", "1.0.0"),
     ]
-
     for version_1, version_2 in to_test:
-        assert Version(version_1) < Version(version_2), f"le failed"
+        assert Version(version_1) < Version(version_2), "le failed"
         assert Version(version_2) > Version(version_1), "ge failed"
         assert Version(version_2) != Version(version_1), "neq failed"
 
